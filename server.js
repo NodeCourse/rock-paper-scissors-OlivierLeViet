1d4b9ca021b46bd499ad23307e57dcd4b5f17644
OlivierLeViet <orsn.tran@free.fr>
2018-05-15 10:09:54 +0200

    33
    33

--- server.js
+++ server.js
@@ -1,47 +1,44 @@
 const express = require('express');
 const randomItem = require('random-item');
-const bodyParser = require('body-parser');
+const choices = require('./dwayne.json');
 const app = express();
 
-const results = ['Égalité !', 'Gagné !', 'Perdu !'];
-const choices = (['paper', 'rock', 'scissors']);
-
-console.log("Server listening : localhost:3000");
-
-const result = null;
-
-function userVsComputer(result, userChoice, computerChoice) {
-
-    if (userChoice === computerChoice) {
-        result = results[0];
-    } else if (userChoice === "rock" && computerChoice === "paper") {
-        result = results[2];
-    } else if (userChoice === "paper" && computerChoice === "rock") {
-        result = results[1];
-    } else if (userChoice === "scissors" && computerChoice === "rock") {
-        result = results[2];
-    } else if (userChoice === "rock" && computerChoice === "scissors") {
-        result = results[1];
-    } else if (userChoice === "paper" && computerChoice === "scissors") {
-        result = results[2];
-    } else if (userChoice === "scissors" && computerChoice === "paper") {
-        result = results[1];
+function getWinner(userChoice, computerChoice) {
+    if (userChoice.winOver.includes(computerChoice.id)) {
+        return userChoice;
     }
 
-    return result;
+    if (computerChoice.winOver.includes(userChoice.id)) {
+        return computerChoice;
+    }
+
+    return null;
 }
 
-
 app.set('view engine', 'pug');
-
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { choices });
 });
 
-app.get('/:choice', (req, res) => {
-    res.render("play", {choice: req.params.choice}, {result: req.params.result});
+app.get('/play/:choice', (req, res) => {
+    const userChoice = choices.find((choice) => {
+        return choice.id === req.params.choice;
+    });
+    const computerChoice = randomItem(choices);
+    const winner = getWinner(userChoice, computerChoice);
+
+    let winnerName;
+    if (winner === userChoice) {
+        winnerName = 'user';
+    } else if (winner === computerChoice) {
+        winnerName = 'computer';
+    }
+
+    res.render('play', { userChoice, computerChoice, winner, winnerName });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
